db37d3072f25fdaadb949dab57dfe61d1c433e71
lostmia <testemail429455@gmail.com>
2024-10-15 21:23:24 +0200

    50 current  2024-10-15 21:23:20  24.05.5667.a3f9ad65a0bf  6.10.1-zen1                          *

--- configuration.nix
+++ configuration.nix
@@ -179,5 +179,16 @@ in {
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
 
+  system.autoUpgrade = {
+    enable = true;
+    flags = [
+      "--update-input"
+      "nixpkgs"
+      "-L"
+    ];
+    dates = "09:00";
+    randomizedDelaySec = "45min";
+  };
+
   system.stateVersion = "24.05"; # Did you read the comment?
 }
