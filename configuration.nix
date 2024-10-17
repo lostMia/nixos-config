2d78d3d3596a8997088e79126a69a55946721b96
lostMia <testemail429455@gmail.com>
2024-10-17 11:59:59 +0200

    81 current  2024-10-17 11:59:56  24.05.5709.c0b1da36f7c3  6.10.1-zen1                          *

--- configuration.nix
+++ configuration.nix
@@ -172,6 +172,10 @@ in {
     python3
   ];
 
+  programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = with pkgs; [
+  ];
+
   environment.variables = {
     GTK_THEME = "Adwaita:dark";
   };
