b8051c5da5eb18e198ccfbc154a416d2b01907f4
lostMia <testemail429455@gmail.com>
2024-10-22 20:08:43 +0200

    143 current  2024-10-22 20:08:35  24.05.5839.a9b86fc2290b  6.10.1-zen1                          *

--- configuration.nix
+++ configuration.nix
@@ -224,6 +224,24 @@ in {
       user = "mia";
       dataDir = "/home/mia/Documents";
       configDir = "/home/mia/Documents/.config/syncthing";
+      overrideDevices = true;
+      overrideFolders = true;
+      settings = {
+        devices = {
+          "android-phone" = {id = "GN7CLEC-K73TLQF-H6KR46Z-7FXJ75F-N4PS4GG-BLLGE3M-SYZWLME-P2ZIIAN";};
+        };
+        folders = {
+          "zepr3-bryuh" = {
+            path = "/home/mia/Password/";
+            devices = ["android-phone"];
+          };
+          "j9zfq-wexpg" = {
+            path = "/home/mia/Notes/";
+            devices = ["android-phone"];
+            ignorePerms = false;
+          };
+        };
+      };
     };
   };
 
