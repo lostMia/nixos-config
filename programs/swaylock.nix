677399e8267141f16e78f7dcf06a28998d975e84
lostmia <testemail429455@gmail.com>
2024-11-25 18:46:59 +0100

    209 current  2024-11-25 18:33:26  24.11.20241125.d249880  6.11.5-zen1                          *

--- programs/swaylock.nix
+++ programs/swaylock.nix
@@ -3,44 +3,44 @@
   pkgs,
   ...
 }: {
-  # programs.swaylock = {
-  #   enable = false;
-  #   settings = {
-  #     daemonize = true;
-  #     show-failed-attempts = true;
-  #     show-keyboard-layout = true;
-  #     ignore-empty-password = true;
-  #     # fingerprint = true;
-  #     image = "$HOME/Pictures/Screenshots/screen.png";
-  #     indicator-radius = 200;
-  #     indicator-thickness = 10;
-  #     inside-color = "#00000000";
-  #     line-color = "#00000000";
-  #     ring-color = "#00000000";
-  #     text-color = "#00000000";
-  #
-  #     layout-bg-color = "#00000000";
-  #     layout-text-color = "#00000000";
-  #
-  #     inside-clear-color = "#00000000";
-  #     line-clear-color = "#00000000";
-  #     ring-clear-color = "#ffff99";
-  #     text-clear-color = "#00000000";
-  #
-  #     inside-ver-color = "#00000000";
-  #     line-ver-color = "#00000000";
-  #     ring-ver-color = "#70ffff";
-  #     text-ver-color = "#00000000";
-  #
-  #     inside-wrong-color = "#00000000";
-  #     line-wrong-color = "#00000000";
-  #     ring-wrong-color = "#ff5555";
-  #     text-wrong-color = "#00000000";
-  #
-  #     bs-hl-color = "#ff5555";
-  #     key-hl-color = "#99ff99";
-  #
-  #     text-caps-lock-color = "#ffffff";
-  #   };
-  # };
+  programs.swaylock = {
+    enable = false;
+    settings = {
+      daemonize = true;
+      show-failed-attempts = true;
+      show-keyboard-layout = true;
+      ignore-empty-password = true;
+      # fingerprint = true;
+      image = "$HOME/Pictures/Screenshots/screen.png";
+      indicator-radius = 200;
+      indicator-thickness = 10;
+      inside-color = "#00000000";
+      line-color = "#00000000";
+      ring-color = "#00000000";
+      text-color = "#00000000";
+
+      layout-bg-color = "#00000000";
+      layout-text-color = "#00000000";
+
+      inside-clear-color = "#00000000";
+      line-clear-color = "#00000000";
+      ring-clear-color = "#ffff99";
+      text-clear-color = "#00000000";
+
+      inside-ver-color = "#00000000";
+      line-ver-color = "#00000000";
+      ring-ver-color = "#70ffff";
+      text-ver-color = "#00000000";
+
+      inside-wrong-color = "#00000000";
+      line-wrong-color = "#00000000";
+      ring-wrong-color = "#ff5555";
+      text-wrong-color = "#00000000";
+
+      bs-hl-color = "#ff5555";
+      key-hl-color = "#99ff99";
+
+      text-caps-lock-color = "#ffffff";
+    };
+  };
 }
