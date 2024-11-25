7afa82cc74ac42cee1b3d2e1f6d4195dd02bbe47
lostmia <testemail429455@gmail.com>
2024-11-25 19:00:30 +0100

    211 current  2024-11-25 18:52:36  24.11.20241125.d249880  6.11.5-zen1                          *

--- modules/pam.nix
+++ modules/pam.nix
@@ -3,12 +3,12 @@
   pkgs,
   ...
 }: {
-  # security.pam.services.swaylock = {
-  #   text = ''
-  #     auth sufficient pam_unix.so try_first_pass likeauth nullok
-  #     auth sufficient pam_fprintd.so
-  #     auth include login
-  #   '';
-  # };
-  # security.pam.services.swaylock.fprintAuth = true;
+  security.pam.services.swaylock = {
+    text = ''
+      auth sufficient pam_unix.so try_first_pass likeauth nullok
+      auth sufficient pam_fprintd.so
+      auth include login
+    '';
+  };
+  security.pam.services.swaylock.fprintAuth = true;
 }
