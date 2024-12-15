261a8a6c3f66de8a0d674116792da720b5a6d1b8
lostmia <testemail429455@gmail.com>
2024-12-15 01:41:21 +0100

    nvidia testing 2

--- hardware/desktop-pc.nix
+++ hardware/desktop-pc.nix
@@ -39,32 +39,42 @@
   ];
 
   hardware.graphics = {
-    extraPackages = with pkgs; [
-      vulkan-tools
-      vulkan-validation-layers
-      libva-utils
-      mesa-utils
-      nvidia-settings
-    ];
+    enable = true;
   };
 
   # Enable NVIDIA proprietary drivers
   services.xserver.videoDrivers = ["nvidia"];
 
-  # Enable Wayland support for NVIDIA
-  hardware.nvidia.modesetting.enable = true;
+  hardware.nvidia = {
 
-  # Enable experimental GBM backend (required for NVIDIA on Wayland)
-  hardware.opengl.enableExperimental = true;
+    # Modesetting is required.
+    modesetting.enable = true;
 
-  # Use the Vulkan driver for better performance
-  hardware.nvidia.package = pkgs.linuxPackages.nvidiaPackages.stable;
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    # Enable this if you have graphical corruption issues or application crashes after waking
+    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+    # of just the bare essentials.
+    powerManagement.enable = false;
 
-  # Add specific options for NVIDIA Wayland
-  environment.variables = {
-    __GLX_VENDOR_LIBRARY_NAME = "nvidia"; # Ensure NVIDIA is used for OpenGL
-    __EGL_VENDOR_LIBRARY_FILENAMES = "${pkgs.nvidia_x11}/share/glvnd/egl_vendor.d/10_nvidia.json";
-    GBM_BACKEND = "nvidia-drm";
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = false;
+
+    # Use the NVidia open source kernel module (not to be confused with the
+    # independent third-party "nouveau" open source driver).
+    # Support is limited to the Turing and later architectures. Full list of 
+    # supported GPUs is at: 
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+    # Only available from driver 515.43.04+
+    # Currently alpha-quality/buggy, so false is currently the recommended setting.
+    open = false;
+
+    # Enable the Nvidia settings menu,
+	  # accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
