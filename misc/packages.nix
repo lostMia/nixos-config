ca0c3959a183ede8746eaf809a3af7c66aad2271
lostmia <testemail429455@gmail.com>
2025-01-05 02:37:28 +0100

    294 current  2025-01-05 02:37:26  24.11.20250102.c1df670  6.12.2-zen1                          *

--- misc/packages.nix
+++ misc/packages.nix
@@ -134,6 +134,10 @@
     ollama = {
       enable = true;
       acceleration = "rocm";
+      environmentVariables = {
+        HCC_AMDGPU_TARGET = "gfx1102"; # used to be necessary, but doesn't seem to anymore
+      };
+      rocmOverrideGfx = "11.0.2";
     };
   };
 
