cd4e43386e054955f4dcff0e33f1f33f407582ac
lostmia <testemail429455@gmail.com>
2025-01-25 14:49:33 +0100

    310 current  2025-01-25 14:49:26  24.11.20250125.ef0c6f2  6.12.10-zen1                          *

--- home.nix
+++ home.nix
@@ -57,21 +57,21 @@
   qt.enable = true;
   qt.style.name = "Dracula";
 
-  gtk = {
-    enable = true;
-    theme = {
-      name = "Graphite-pink-Dark";
-      package = pkgs.graphite-gtk-theme;
-    };
-    iconTheme = {
-      name = "Tela-black";
-      package = pkgs.tela-icon-theme;
-    };
-    cursorTheme = {
-      name = "Graphite dark Cursors";
-      package = pkgs.graphite-cursors;
-    };
-  };
+  # gtk = {
+  #   enable = true;
+  #   theme = {
+  #     name = "Graphite-pink-Dark";
+  #     package = pkgs.graphite-gtk-theme;
+  #   };
+  #   iconTheme = {
+  #     name = "Tela-black";
+  #     package = pkgs.tela-icon-theme;
+  #   };
+  #   cursorTheme = {
+  #     name = "Graphite dark Cursors";
+  #     package = pkgs.graphite-cursors;
+  #   };
+  # };
 
   home.pointerCursor = {
     gtk.enable = true;
@@ -80,11 +80,11 @@
     size = 64;
   };
 
-  dconf.settings = {
-    "org/gnome/desktop/interface" = {
-      color-scheme = "prefer-dark";
-    };
-  };
+  # dconf.settings = {
+  #   "org/gnome/desktop/interface" = {
+  #     color-scheme = "prefer-dark";
+  #   };
+  # };
 
   home.file.".mozilla/firefox/mia/chrome/".source = let
     cascade = pkgs.callPackage ./src/cascade/cascade.nix {};
