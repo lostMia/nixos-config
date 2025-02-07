1579141a0046e6671e7b1503b81ae572f64569d5
lostmia <testemail429455@gmail.com>
2025-02-07 16:53:36 +0100

    329 current  2025-02-07 16:33:49  24.11.20250125.ef0c6f2  6.12.10-zen1                          *

--- programs/shikane.nix
+++ programs/shikane.nix
@@ -42,6 +42,27 @@
       scale = 1.0
       transform = "normal"
       adaptive_sync = false
+
+      [[profile]]
+      name = "htlec2"
+
+      [[profile.output]]
+      enable = true
+      search = ["m=0x0BCA", "s=", "v=BOE"]
+      mode = "2256x1504@59.999Hz"
+      position = "1317,1363"
+      scale = 1.0
+      transform = "normal"
+      adaptive_sync = false
+
+      [[profile.output]]
+      enable = true
+      search = ["m=S22C450", "s=H4MG202424", "v=Samsung Electric Company"]
+      mode = "1680x1050@59.954Hz"
+      position = "1595,313"
+      scale = 1.0
+      transform = "normal"
+      adaptive_sync = false
     '';
   };
 }
