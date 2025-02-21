7a4bee2b43b7ed50a8ca5f00ae7b62fe66494fe4
lostmia <testemail429455@gmail.com>
2025-02-21 18:28:28 +0100

    345 current  2025-02-21 18:28:26  24.11.20250125.ef0c6f2  6.12.10-zen1                          *

--- services/cron.nix
+++ services/cron.nix
@@ -3,7 +3,6 @@
     enable = true;
     systemCronJobs = [
       "0 0 * * Sat  mia  bash /home/mia/.config/nixos/scripts/SL_saturday_my_dudes.sh"
-      "* * * * *  mia  bash /home/mia/.config/nixos/scripts/SL_saturday_my_dudes.sh"
     ];
   };
 }
