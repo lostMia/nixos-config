aca93d83f7e82017b0a058b5509fa87aeb476814
lostmia <testemail429455@gmail.com>
2025-03-30 19:00:37 +0200

    419 current  2025-03-30 19:00:34  24.11.20250225.71799de  6.12.10-zen1                          *

--- modules/network.nix
+++ modules/network.nix
@@ -57,7 +57,7 @@
     wg0 = {
       # Determines the IP address and subnet of the client's end of the tunnel interface.
       ips = ["192.168.1.200/24"];
-      listenPort = 21975;
+      listenPort = 51820;
       privateKeyFile = "/home/mia/Documents/Wireguard/private";
 
       peers = [
