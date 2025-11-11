2b5a0158b60c5065befc3176a9bfa1f22432c86b
lostmia <miaunterladstaetter@gmail.com>
2025-11-11 09:23:38 +0100

    640 current  2025-11-11 09:23:36  25.05.20251019.33c6dca  6.17.2-zen1                          *

--- modules/cups.nix
+++ modules/cups.nix
@@ -1,6 +1,13 @@
 {pkgs, ...}: {
   services.printing = {
     enable = true;
-    drivers = with pkgs; [gutenprint cnijfilter2 cnijfilter_4_00 cnijfilter_2_80];
+    drivers = with pkgs; [
+      gutenprint
+      cnijfilter2
+      cnijfilter_4_00
+      cnijfilter_2_80
+      labelife-label-printer
+      lprint
+    ];
   };
 }
